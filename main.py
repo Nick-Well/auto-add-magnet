3529d0f9b5fc7da72a4277e5f2f41643fb10f7b6
root <nick.wellsing@tuta.io>
2023-06-19 12:15:52 +0200

    stream line testing

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ docomentery = "/doc"
 movies = "/movies"
 folder = "/"
 
+def clean_text(text):
+    pattern = r'S\d{2}E\d{2}'
+    cleaned_text = re.findall(pattern, text)
+    return cleaned_text
+
 
 def addName():
     name = input("add name: ") or 0
@@ -30,21 +35,16 @@ def addName():
         print("needs a name")
     else:
         foldNr = int(input("folder 1.tv-shows 2.movies 3.docomentery: "))
-        print(foldNr)
-        match foldNr:
-            case 1:
-                folder = shows
-                print(folder)
-            case 2:
-                folder = movies
-                print(folder)
-            case 3:
-                folder = docomentery
-                print(folder)
-            case _:
-                return folder
+        if foldNr == 1:
+            folder = shows
+        elif foldNr == 2:
+            folder = movies
+        elif foldNr == 3:
+            folder == docomentery
+        else:
+            print("nofolder")
         downloadFile = open("download.txt", "a")
-        downloadFile.write(folder + " " + name + " x00x00 " + "\n")
+        downloadFile.write(folder + " " + name + "\n")
         downloadFile.close()
 
 
@@ -52,15 +52,23 @@ def lookForName():
     with open("download.txt", "r") as f:
         for line in f:
             line = line.split()
-            print(line)
-            files = glob.glob("/mnt/HDD1" +line[0] + "/" + line[1] +"/*")
-            print(files)
-    downloadFile.close()
+            print(line[0]+" "+line[1])
+            oneStep = str("/mnt/HDD1" +line[0] + "/*" + line[1] +"*/*.mkv")
+            inLocation = str("/mnt/HDD1" +line[0] + "/*" + line[1] +"*.mkv")
+            
+            filesF = glob.glob(oneStep)
+            for file in filesF:
+                print(clean_text(file))
+            
+            files = glob.glob(inLocation)
+            for file in files:
+                print(clean_text(file))
 
 def getMagnet():
     with open("download.txt", "r") as f:
         for line in f:
             name = line.split()
+            print(name[1])
             first = "https://www.magnetdl.org"
             URL = "https://www.magnetdl.org/s/" + name[1] + "-1080p-h264/se/desc/"
 
@@ -68,14 +76,21 @@ def getMagnet():
                 session.headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
                 page = requests.get(first)
                 response = session.get(URL, headers={"Accept" : "application/json, text/javascript, */*; q=0.01", "X-Requested-With": "XMLHttpRequest", "Referer": "https://www.magnetdl.org", "Host": "www.magnetdl.org"})
+                soup = BeautifulSoup(response.content, "lxml")
+                for link in soup.find_all('a',attrs={'href': re.compile("^magnet:/?.*"+name[1], re.IGNORECASE)}):
+                    print(link.get('href'))
+                    print(link)
+while True:
+    foldNr = input("1.getMagnet 2.lookForName 3.addName: ")
+    if foldNr == "1":
+        getMagnet()
+    elif foldNr == "2":
+        lookForName()
+    elif foldNr == "3":
+        addName()
+    else:
+        break
 
-            soup = BeautifulSoup(response.content, "lxml")
-            for link in soup.find_all('a',attrs={'href': re.compile("^magnet:/?.*"+name[1]+".*S01E08", re.IGNORECASE)}):
-                print(link.get('href'))
-
-getMagnet()
-#lookForName()
-#addName()
 
 
 
