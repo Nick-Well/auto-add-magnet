09dbcee64f49399f8708bf031514df4d2cfba5f2
root <nick.wellsing@tuta.io>
2023-06-20 19:09:33 +0200

    added some functions

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import glob
 import re
+from transmission_rpc import Client
 from bs4 import BeautifulSoup
 
 #TODO: Make a file with array: name (%00s %00e/complete) H264 %folder %DONE
 
-#TODO: Check if file exist: error
 #TODO: Check if name already exist on hardrive and add to the file if already exist
 #TODO: imdb check epesods left too modify %DONE 
 #TODO:      add date wen next epesode is out and skip if date doesnt match/biger  
@@ -15,17 +15,33 @@ from bs4 import BeautifulSoup
 #TODO: Loop array: magnet %folder in to Transmission
 
 #TODO: stages so no need to repeat it every time 
+
+#TODO: only open files ones and not for every call
+#TODO: change the global veriables to classes
+#TODO: folder information also to an array/ class object for easyer calls 
 #TODO: 
-#TODO: 
+
 
 shows = "/tv"
 docomentery = "/doc"
 movies = "/movies"
 folder = "/"
+mountPoint = "/mnt/HDD1"
+    #print("defult: /transmission/")
+    #webInterface = input ("webinterface: ")
+
+def serverIp():
+    ip = input("whats the ip of the Transmission server: ")
+    print("defult: 9091")
+    port = input("whats the port of the Transmission server: ") or 9091
+    infoFile = open("localInfo", "a")
+    infoFile.write(ip+" "+port)
+    infoFile.close()
+
 
 def clean_text(text):
     pattern = r'S\d{2}E\d{2}'
-    cleaned_text = re.findall(pattern, text)
+    cleaned_text = re.findall(pattern, text, re.IGNORECASE)
     return cleaned_text
 
 
@@ -48,30 +64,50 @@ def addName():
         downloadFile.close()
 
 
+#TODO: split this up to 2 functions if not 3
 def lookForName():
+    writeLines = []
     with open("download.txt", "r") as f:
+        lineNr = 0
         for line in f:
+            testWord = 0
+            lastEp = "S00E00"
             line = line.split()
-            print(line[0]+" "+line[1])
-            oneStep = str("/mnt/HDD1" +line[0] + "/*" + line[1] +"*/*.mkv")
-            inLocation = str("/mnt/HDD1" +line[0] + "/*" + line[1] +"*.mkv")
+            print(mountPoint + line[0]+" "+line[1])
+            while testWord != 2:
+                if testWord == 1:
+                    name = line[1]
+                    lineU = name[0].upper() + name[1:]
+                    files = glob.glob(mountPoint + line[0] + "/**/*" + lineU +"*.mkv", recursive=True)
+                    print(lineU)
+                    for file in files:
+                        text = clean_text(file)
+                        if len(text) != 0:
+                            if lastEp < text[0]:
+                                lastEp = text[0]
+                files = glob.glob(mountPoint + line[0] + "/**/*" + line[1] +"*.mkv", recursive=True)
+                testWord+=1
+                for file in files:
+                    text = clean_text(file)
+                    if len(text) != 0:
+                        if lastEp < text[0]:
+                            lastEp = text[0]
+            print(lineNr)
+            print(lastEp)
+            writeLines.append(str(line[0] +" "+ line[1] +" "+ lastEp +"\n").lower())
+            lineNr+=1
+    with open("download.txt", "w") as w:
+        w.writelines(writeLines)
             
-            filesF = glob.glob(oneStep)
-            for file in filesF:
-                print(clean_text(file))
             
-            files = glob.glob(inLocation)
-            for file in files:
-                print(clean_text(file))
-
 def getMagnet():
     with open("download.txt", "r") as f:
         for line in f:
             name = line.split()
             print(name[1])
             first = "https://www.magnetdl.org"
-            URL = "https://www.magnetdl.org/s/" + name[1] + "-1080p-h264/se/desc/"
-
+            URL = "https://www.magnetdl.org/"+ name[1][0] +"/" + name[1] +"-"+ name[2] +"-1080p-h264/se/desc/"
+            print(URL)
             with requests.Session() as session:
                 session.headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
                 page = requests.get(first)
@@ -79,18 +115,21 @@ def getMagnet():
                 soup = BeautifulSoup(response.content, "lxml")
                 for link in soup.find_all('a',attrs={'href': re.compile("^magnet:/?.*"+name[1], re.IGNORECASE)}):
                     print(link.get('href'))
-                    print(link)
-while True:
-    foldNr = input("1.getMagnet 2.lookForName 3.addName: ")
-    if foldNr == "1":
-        getMagnet()
-    elif foldNr == "2":
-        lookForName()
-    elif foldNr == "3":
-        addName()
-    else:
-        break
 
+def start():
+    while True:
+        x = input("1.getMagnet 2.lookForName 3.addName 4.addTransmissionIp: ")
+        if x == "1":
+            getMagnet()
+        elif x == "2":
+            lookForName()
+        elif x == "3":
+            addName()
+        elif x == "4":
+            serverIp()
+        else:
+            break
 
+start()
 
 
